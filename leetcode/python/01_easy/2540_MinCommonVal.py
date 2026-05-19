1d0e876c794a2b8e9ac76787a01b9f5ceb6f30f6
Nikhil Singla <nsingla3.14@gmail.com>
2026-05-18 19:27:47 -0500

    [A1] First attempt by utilizing properties of the datastructure

---
+++ leetcode/python/01_easy/2540_MinCommonVal.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1 = set(nums1)
+        for i in nums2:
+            if i in nums1:
+                return i
+        
+        return -1
