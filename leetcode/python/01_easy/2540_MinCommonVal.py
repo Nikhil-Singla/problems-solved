b15994367cb5e2e3da982a4ce95411d1bf76f8b0
Nikhil Singla <nsingla3.14@gmail.com>
2026-05-19 01:53:54 -0500

    [A2] [SO] [MF] Optimized for speed by early break conditions and piece by piece evaluation

--- leetcode/python/01_easy/2540_MinCommonVal.py
+++ leetcode/python/01_easy/2540_MinCommonVal.py
@@ -1,8 +1,19 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1 = set(nums1)
-        for i in nums2:
-            if i in nums1:
-                return i
+        if nums1[-1] < nums2[0]:
+            return -1
+        if nums2[-1] < nums1[0]:
+            return -1
+
+        n, m = len(nums1), len(nums2)
+        idx1, idx2 = 0, 0
+
+        while (idx1 < n) and (idx2 < m):
+            if nums1[idx1] == nums2[idx2]:
+                return nums1[idx1]
+            elif nums1[idx1] < nums2[idx2]:
+                idx1 += 1
+            else:
+                idx2 += 1
         
         return -1
